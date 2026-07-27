34d5685fadc3995a01b3f88d926cbb09c6362d8b
Shain Anderson Alir Chino Calle <schinoc@unjbg.edu.pe>
2026-07-27 18:13:34 -0500

    Actualiza el triangulo de caracteres

--- Altura de un Triángulo.cpp
+++ Altura de un Triángulo.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 using namespace std;
-int main (){
-	
-	int altura;
-	
-	cout<<"Ingrese la altura del triangulo: ";
-	cin>>altura;
-	
-	for (int i = 1; i <= altura; i++){
-		for (int j = 1; j <= i; j++){
-			cout<<"* ";
-		}
-		cout<<endl;
-	}
-	
-	return 0;
+
+int main() {
+
+    int altura;
+    char simbolo;
+
+    cout << "Ingrese la altura del triangulo: ";
+    cin >> altura;
+
+    cout << "Ingrese el simbolo que desea usar: ";
+    cin >> simbolo;
+
+    if (altura <= 0) {
+        cout << "La altura debe ser mayor que 0." << endl;
+    }
+    else {
+
+        cout << "\nTriangulo generado:\n\n";
+
+        for (int i = 1; i <= altura; i++) {
+
+            for (int j = altura; j > i; j--) {
+                cout << " ";
+            }
+
+            for (int j = 1; j <= i; j++) {
+                cout << simbolo << " ";
+            }
+
+            cout << endl;
+        }
+
+    }
+
+    return 0;
 }
