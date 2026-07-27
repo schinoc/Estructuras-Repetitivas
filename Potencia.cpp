f5655cfe0ec422506fda841056a2e43ce93b3da0
Shain Anderson Alir Chino Calle <schinoc@unjbg.edu.pe>
2026-07-27 18:19:03 -0500

    Actualiza potencia

--- Potencia.cpp
+++ Potencia.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 using namespace std;
-int main (){
-	
-	int base, exponente;
-	long resultado = 1;
-	
-	cout<<"introduce la base: ";
-	cin>>base;
-	cout<<"introduce el exponente: ";
-	cin>>exponente;
-	
-	for (int i = 1; i <= exponente; i++){
-		resultado = resultado * base;
-	}
-	
-	cout<<base<<" elevado a "<<exponente<<" es: "<<resultado<<endl;
-	
-	return 0;
+
+int main() {
+
+    int base, exponente;
+    long resultado = 1;
+
+    cout << "Ingrese la base: ";
+    cin >> base;
+
+    cout << "Ingrese el exponente: ";
+    cin >> exponente;
+
+    if (exponente < 0) {
+        cout << "Exponente no valido";
+    }
+    else {
+        for (int i = 1; i <= exponente; i++) {
+            resultado *= base;
+        }
+
+        cout << base << "^" << exponente << " = " << resultado << endl;
+    }
+
+    return 0;
 }
